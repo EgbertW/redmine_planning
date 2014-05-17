47519a75148295d90cef4c224ca660d3d997c253
Egbert van der Wal <ewal@pointpro.nl>
2014-05-17 09:52:31 +0200

    added gpl-v3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
+# Copyright 2014 Egbert van der Wal <egbert@assistobot.com>
+# 
+# This file is part of redmine_planning
+#
+# redmine_planning is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# redmine_planning is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with redmine_planning. If not see <http://www.gnu.org/licenses/>.
+
 RedmineApp::Application.routes.draw do
-#  post '/issues/:id/move', :to => 'issues#move', :id => /\d+/, :as => 'move_issue'
   post '/issues/:id/move', :to => 'moves#move', :id => /\d+/, :as => 'move_issue'
   post '/projects/:project_id/plan', :to => 'planning#save', :id => /\w+/, :as => 'save_planning_project'
   get '/projects/:project_id/plan', :to => 'planning#show', :id => /\w+/, :as => 'planning_project'
